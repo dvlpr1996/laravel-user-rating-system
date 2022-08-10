3fabdd62634bca97bd235bfa5f8d58ddb317a5da
dvlpr1996 <nimajahanbakhshian@gmail.com>
2022-08-10 16:00:29 +0430

    implement Models Eloquent relationships

---
+++ app/Models/Badge.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Badge extends Model
+{
+	use HasFactory;
+	protected $table = 'badges';
+
+	// public function badge_users()
+	// {
+	// 	return $this->hasMany(Badge_User::class);
+	// }
+}
---
+++ app/Models/Topic.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Topic extends Model
+{
+	use HasFactory;
+	protected $table = 'topics';
+
+	public function user()
+	{
+		return $this->belongsTo(User::class);
+	}
+
+	public function answers()
+	{
+		return $this->hasMany(Answer::class);
+	}
+}
