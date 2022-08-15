7199f3741a10c6a498f72a5b92e0a95746d04875
dvlpr1996 <nimajahanbakhshian@gmail.com>
2022-08-16 00:00:14 +0430

    install realrashid/sweet-alert

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -18,8 +18,8 @@
 		<link rel="apple-touch-icon" sizes="180x180" href="...">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 		@vite('resources/css/app.css')
+		@include('sweetalert::alert')
 		<!-- [if lt IE 9]>
 			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
 			<![endif] -->
 </head>
-
