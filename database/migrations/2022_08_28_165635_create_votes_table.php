cddda831b68749d64e80374da072f14069e6fb2d
dvlpr1996 <nimajahanbakhshian@gmail.com>
2022-08-29 17:54:51 +0430

    implement vote migration

---
+++ database/migrations/2022_08_28_165635_create_votes_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+	public function up()
+	{
+		Schema::create('votes', function (Blueprint $table) {
+			$table->bigIncrements('id');
+			$table->foreignId('answer_id')->constrained('answers');
+			$table->tinyInteger('vote')->default(0);
+			$table->timestamps();
+		});
+	}
+
+	public function down()
+	{
+		Schema::dropIfExists('votes');
+	}
+};
